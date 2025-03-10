c1d8a8a2045ec595758d456837902aacc71d0b47
Thanapat Thipthiangthae <caveman.pk@gmail.com>
2025-03-10 15:31:40 +0700

    Idle Animation
    - add idle animation
    - add direction variable
    - add idlesAnimLeft[] and idlesAnimRight[]

--- Main/Player.java
+++ Main/Player.java
@@ -4,19 +4,22 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Player {
-//    final int animDelay = 10;
-//    int currentFrame = 0;
-//    int
+    final int animDelay = 7;
+    int currentFrame = 0;
+    int currentIdleFrame = 0;
+    String direction;
 
     int x, y, speed;
     KeyHandler keyH;
     GamePanel gp;
     Image hansel;
-    Image idlesAnim[];
+    Image idlesAnimLeft[];
+    Image idlesAnimRight[];
 
     public Player(GamePanel gamePanel, KeyHandler keyH) {
         this.gp = gamePanel;
         this.keyH = keyH;
+        direction = "left";
         valuesSetting();
         playerLoading();
     }
@@ -25,43 +28,70 @@ public class Player {
         x = gp.mapX / 2;
         y = gp.mapY / 2;
         speed = 5;
-        idlesAnim = new Image[9];
+        idlesAnimLeft = new Image[9];
+        idlesAnimRight = new Image[9];
     }
 
     public void playerLoading() {
         for (int i = 0; i <= 8; i++) { //Storing player.png in an array
-            String path = "res/Character/Hansel/Hansel_Idle/hansel_idle"+(i+1)+".png";
-            idlesAnim[i] = new ImageIcon(path).getImage();
+            String path = "res/Character/Hansel/Hansel_Idle/hansel_idle_left"+(i+1)+".png";
+            idlesAnimLeft[i] = new ImageIcon(path).getImage();
         }
-        hansel = new ImageIcon("res/Character/Hansel/Hansel_Idle/hansel_idle1.png").getImage();
+        for (int i = 0; i <= 8; i++) { //Storing player.png in an array
+            String path = "res/Character/Hansel/Hansel_Idle/hansel_idle_right"+(i+1)+".png";
+            idlesAnimRight[i] = new ImageIcon(path).getImage();
+        }
     }
 
     public void animationHandler() {
+        currentFrame++;
+        if (currentFrame >= animDelay) {
+            currentFrame = 0;
+            currentIdleFrame++;
+        }
 
+        if (currentIdleFrame > idlesAnimLeft.length-1) {
+            currentIdleFrame = 0;
+        }
+
+        if (direction.equals("left")) {
+            hansel = idlesAnimLeft[currentIdleFrame];
+        } else if (direction.equals("right")) {
+            hansel = idlesAnimRight[currentIdleFrame];
+        }
     }
 
     public void update() {
         int speedDiag = (int) (speed/Math.sqrt(2));
+
         if (keyH.upPressed && keyH.rightPressed) {
             x += speedDiag;
             y -= speedDiag;
+            direction = "right";
         } else if (keyH.upPressed && keyH.leftPressed) {
             x -= speedDiag;
             y -= speedDiag;
+            direction = "left";
         } else if (keyH.downPressed && keyH.rightPressed) {
             x += speedDiag;
             y += speedDiag;
+            direction = "right";
         } else if (keyH.downPressed && keyH.leftPressed) {
             x -= speedDiag;
             y += speedDiag;
+            direction = "left";
         } else if (keyH.upPressed) {
             y -= speed;
+            direction = "up";
         } else if (keyH.downPressed) {
             y += speed;
+            direction = "down";
         } else if (keyH.leftPressed) {
             x -= speed;
+            direction = "left";
         } else if (keyH.rightPressed)  {
             x += speed;
+            direction = "right";
         }
         animationHandler();
     }
