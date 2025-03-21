4762c33f16a2968aba36ef78bd4d45eca43bee95
Au4uztOLsen <pacharanatthakorn@gmail.com>
2025-03-21 16:03:48 +0700

    test Gra in map

--- Main/MapManager.java
+++ Main/MapManager.java
@@ -1,35 +1,51 @@
 package Main;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class MapManager {
     GamePanel gp;
     Player player;
+    String path;
+    Image idleImage;
 
     public MapManager(GamePanel gp, Player player) {
         this.gp = gp;
         this.player = player;
+        path = "res/Character/Gratel/Gratel_Idle/gratel_idle1.png";
+        idleImage = new ImageIcon(path).getImage();
     }
 
-    public void drawMap(Graphics g){
+    public void drawMap(Graphics g) {
         int worldX;
         int worldY;
         int screenX;
         int screenY;
+
         for (int row = 0; row < gp.maxRow; row++) {
             for (int col = 0; col < gp.maxCol; col++) {
                 int tile = gp.map[row][col];
                 worldX = col * gp.xTileSize;
                 worldY = row * gp.yTileSize;
-//                screenX = worldX - player.worldX + player.screenX;
-//                screenY = worldY - player.worldY + player.screenY;
                 screenX = worldX - player.worldX;
                 screenY = worldY - player.worldY;
-                if (screenX + gp.xTileSize >= 0 && screenX <= GamePanel.mapX && //screenY + gp.yTileSize >= 0 คือเหมือนเพิ่มขอบไป 1 tile size จะได้ render ทัน ไม่ให้เกิดขอบดำ
+
+                if (screenX + gp.xTileSize >= 0 && screenX <= GamePanel.mapX &&
                         screenY + gp.yTileSize >= 0 && screenY <= GamePanel.mapY) {
                     g.drawImage(gp.tileMap.getTileImage(tile), screenX, screenY, gp.xTileSize, gp.yTileSize, null);
                 }
             }
         }
+
+        int fixedX = 25;
+        int fixedY = gp.maxRow * gp.yTileSize - gp.yTileSize;
+
+        int screenIdleX = fixedX - player.worldX + player.screenX;
+        int screenIdleY = fixedY - player.worldY + player.screenY;
+
+        System.out.println(screenIdleX+"X");
+        System.out.println(screenIdleY+"Y");
+
+        g.drawImage(idleImage, screenIdleX, screenIdleY-450, gp.xTileSize*2, gp.yTileSize*2, null);
     }
 }
